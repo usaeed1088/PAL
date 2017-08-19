abb314ae18ca11b4a8a5fc861b9b4fffb33d21df
usaeed1088 <usaeed1088@gmail.com>
2017-08-18 23:28:26 -0700

    Making Unix API constructors public as a workaround to enable build with g++

--- Lib/API/Unix/Trance/Trance.h
+++ Lib/API/Unix/Trance/Trance.h
@@ -10,7 +10,17 @@ namespace PAL
 		{
 			class Trance : public ITrance
 			{
-			private:
+				// NOTE: Constructor should and must be private and only
+				// be accessible by Factory class. However, g++ complains
+				// "error: ‘PAL::API::Unix::Time::Time()’ is private" because
+				// it compiles Factory class after compiling this class and
+				// hence, cannot associate friendship between both. MSVC is happy
+				// with this constructor being private.
+				// Also, this note should be pasted on every Unix API class until
+				// this is resolved
+				// TODO: Find a workaround
+			//private:
+			public:
 				friend class Factory;
 				Trance();
 
